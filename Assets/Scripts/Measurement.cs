12018e1da44c652d20e7bfcbb3653d9e6db3b94c
Nick <nick.borchers@gmail.com>
2016-08-10 21:32:24 -0400

    Began adding TrackManager.cs and Measurement.cs

---
+++ Assets/Scripts/Measurement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+class Measurement : MonoBehaviour
+{
+    public GameObject target { get; private set; }
+    private Rigidbody rb;
+
+    public Vector3 position;
+    public Vector3 velocity;
+
+    public Vector3 posSigma;
+    public Vector3 velSigma;
+
+    void Start()
+    {
+        rb = target.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        pseudoMeasurement();
+    }
+
+    // This method fills the role of getting the target measuremnt.
+    // In the actual implementation, this will read measurements from the sensors.
+    // Here, simply query the measuremetns of the target GameObject.
+    void pseudoMeasurement()
+    {
+        // Measured position is the targets global position
+        position = target.transform.position;
+
+        // Pull the target's rigidbody to find velocity
+        velocity = rb.velocity;
+
+        // Since this measurement is essentially truth, set the uncertainties to zero.
+        posSigma = Vector3.zero;
+        velSigma = Vector3.zero;
+    }
+}
+
---
+++ Assets/Scripts/TrackManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TrackManager : MonoBehaviour
+{
+    public static TrackManager Instance { get; private set; }
+
+    // Represent the object which is being tracked.
+    // public GameObject TrackedObject { get; private set; }
+
+    // Vectors for the track state
+    public Vector3 trackPosition;
+    public Vector3 trackVelocity;
+
+    // State uncertainties
+    public Vector3 trackPosSigma;
+    public Vector3 trackVelSigma;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Collect a measurement from the target.
+    }
+}
+
