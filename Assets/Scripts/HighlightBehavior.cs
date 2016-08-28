60ba25ce59ecd70974847db812a09735cbe8723a
Nick <nick.borchers@gmail.com>
2016-08-28 06:19:26 -0400

    Updated HighlightBehavior colors: Incoming: Red, Outgoing: Blue, Neutral: Yellow

--- Assets/Scripts/HighlightBehavior.cs
+++ Assets/Scripts/HighlightBehavior.cs
@@ -17,7 +17,7 @@ public class HighlightBehavior : MonoBehaviour
         this.transform.position = TrackManager.Instance.trackPosition;
 
         // take the dot product of the target velocity and user gaze to determine range rate
-        var targetVelocity = MeasurementManager.Instance.measVel.normalized;
+        var targetVelocity = TrackManager.Instance.trackVelocity.normalized;
         var gazeDirection = Camera.main.transform.forward;
         var align = Vector3.Dot(targetVelocity, gazeDirection);
 
@@ -25,7 +25,7 @@ public class HighlightBehavior : MonoBehaviour
         Color color = Color.black;
         color.r = 1 - ((align >= 0) ? align : 0);
         color.g = 1 - ((align >= 0) ? align / 2 : -align);
-        color.b = 1 + ((align >= 0) ? 0 : align);
+        color.b = 0 + ((align >= 0) ? align : 0);
 
         // Change light to match chosen color
         Light light = this.gameObject.GetComponent<Light>();
