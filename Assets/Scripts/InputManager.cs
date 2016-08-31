817c510b42d605d5bb9a945e1c78d8ab3335b7ec
Nick <nick.borchers@gmail.com>
2016-08-31 19:05:31 -0400

    Added InputManager.cs to help with debugging within Unity. Started Adding Trajectory support.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+
+    bool running = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.anyKeyDown)
+            if (!running)
+            {
+                this.BroadcastMessage("OnDrop");
+                this.BroadcastMessage("OnTrack");
+                running = true;
+            }
+            else
+            {
+                this.BroadcastMessage("OnReset");
+                running = false;
+            }
+	
+	}
+}
