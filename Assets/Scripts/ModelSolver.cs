a19329b741e3deda8b62710e4906f5b5e4f422f8
Nick <nick.borchers@gmail.com>
2016-08-31 20:13:31 -0400

    Added ModelSolver.cs to calculate trajectory solution positions. Added simple test solver to start with.

---
+++ Assets/Scripts/ModelSolver.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelSolver : MonoBehaviour
+{
+    public Vector3 separation;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Form a transform array of solution points
+        Transform[] points = GetComponentsInChildren<Transform>();
+
+        testSolver(ref points);
+        
+	}
+
+    void testSolver(ref Transform[] pnt)
+    {
+        pnt[1].position = TrackManager.Instance.trackPosition;
+
+        for (int i = 2; i < pnt.Length; i++)
+            pnt[i].position = pnt[i - 1].position + separation;
+    }
+}
